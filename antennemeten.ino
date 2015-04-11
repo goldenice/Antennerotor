ab8e541b2d590167ce6aff15259c0aae172d8a50
Rick Lubbers <ik@ricklubbers.nl>
2015-04-11 13:16:56 +0200

    Rewritten all the code, but it fully works now :)

--- antennemeten.ino
+++ antennemeten.ino
@@ -1,228 +1,186 @@
-/**
- * TODOlist before this script can be used:
- * - Implement turning
- * - Implement the display
- * - Set defines to the correct pins and correct dswitch_state[]'s
- *
- */
+#define SW_TURN_LEFT 5 
+#define SW_TURN_RIGHT 6
 
-/****************************************************************************************/
-/***************************  STATIC DEFINES        *************************************/
-/****************************************************************************************/
+#define SW_THDB_LEFT 10
+#define SW_THDB_RIGHT 7
 
-// These are the dstates, not the actual pins!
-#define SW_TURN_LEFT = 1;   // turn left
-#define SW_TURN_RIGHT = 2;  // turn right
+#define SW_ZERO 8
+#define SW_OET 4
+#define SW_KILL 9
 
-#define SW_THDB_LEFT = 0;   // 3db left
-#define SW_THDB_RIGHT = 0;  // 3db right
+#define OUT_CW 12
+#define OUT_PULSE 11
 
-#define SW_ZERO = 0;        // reset dynamic degrees
-#define SW_OET = 0;         // 180 deg turn
-#define SW_KILL = 0;        // kill switch
+// Defining the struct to read the current state of the switches in
+struct SwitchState {
+  boolean turnleft;
+  boolean turnright;
+  boolean thdbleft;
+  boolean thdbright;
+  boolean zero;
+  boolean oet;
+  boolean kill;
+};
+SwitchState switches = (SwitchState) { false, false, false, false, false, false, false };
 
-#define PULSE_PIN = 11;
-#define CW_PIN = 12;
+// Defining the timers used in the program
+struct Timer {
+  unsigned long last;
+  unsigned long interval;
+  boolean micro;
+};
+Timer timer[10];
 
-/****************************************************************************************/
-/***************************  LIBRARIES             *************************************/
-/****************************************************************************************/
+int pulsePin = LOW;
 
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+struct TurnState {
+  boolean turnlong;
+  boolean left;
+  boolean right;
+  float angle;
+  float offset;
+  float target;
+  float thdbleft;
+  float thdbright;
+};
+TurnState turnstate = (TurnState) { false, false, false, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
-
-/****************************************************************************************/
-/***************************  GLOBAL VARIABLES      *************************************/
-/****************************************************************************************/
-
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-
-// Digital switches and their states
-int dswitches[9] =              {  9,  5,  6,  4,  8, 10,  7,  A0,  A1 };
-int dswitch_state[9] =          {  0,  0,  0,  0,  0,  0,  0,   0,   0 };
-int dswitch_state_old[9] =      {  0,  0,  0,  0,  0,  0,  0,   0,   0 };
-
-// The timers used throughout the program
-unsigned long timer_last[5] =   {   0,   0,   0,   0,   0 };
-unsigned long timer_diff[5] =   { 200,   0,   0,   0,   0 };
-
-// Microtimer: does the same as the main timer, but based on microseconds
-unsigned long microtimer_last[5] = {   0,   0,   0,   0,   0 };
-unsigned long microtimer_diff[5] = {8000,   0,   0,   0,   0 };
-
-// State of the engine
-bool longturn = false;
-bool turning_left = false;
-bool turning_right = false;
-float offset = 0.0;
-float angle = 0.0;
-float target = 0.0;
-bool pulseState = false;
-
-
-/****************************************************************************************/
-/***************************  MAIN FUNCTIONS        *************************************/
-/****************************************************************************************/
-
-/**
- * Setup function: is run once before loop is run continuously
- */
 void setup() {
-    // Set input mode for all digital switches
-    for (int i = 0; i < (sizeof(dswitches)/sizeof(int)); i++) {
-        pinMode(dswitches[i], INPUT);
-    }
-
-    pinMode(PULSE_PIN, OUTPUT);
-    pinMode(CW_PIN, OUTPUT);
-
-    lcd.init();
-    lcd.backlight();
-    lcd.clear();
+  pinMode(SW_TURN_LEFT, INPUT);
+  pinMode(SW_TURN_RIGHT, INPUT);
+  pinMode(SW_THDB_LEFT, INPUT);
+  pinMode(SW_THDB_RIGHT, INPUT);
+  pinMode(SW_ZERO, INPUT);
+  pinMode(SW_OET, INPUT);
+  pinMode(SW_KILL, INPUT);
+  
+  pinMode(OUT_CW, OUTPUT);
+  pinMode(OUT_PULSE, OUTPUT);
+  
+  timer[0] = { micros(), 8000, true };
+  timer[1] = { millis(), 100, false };
+  timer[2] = { millis(), 200, false };
+  
+  Serial.begin(38400);
 }
 
-/**
- * Main loop
- */
 void loop() {
-    if (checkMicroTimer(0)) {
-        resetMicroTimer(0);
-        doPulse();
+  if (checkTimer(0)) {
+    if (turnstate.left || turnstate.right || pulsePin == HIGH){
+      pulse();
     }
-
-    if (checkTimer(0)) {
-        resetTimer(0);
-        refreshInputDigital();
-        refreshInputAnalog();
-        interpretInput();
-    }
+  }
+  if (checkTimer(1)) {
+    refreshInput();
+  }
+  if (checkTimer(2)) {
+    output();
+  }
 }
 
-/****************************************************************************************/
-/***************************  INPUT READING         *************************************/
-/****************************************************************************************/
-
-/**
- * Refreshes input on the digital pins
- */
-void refreshInputDigital() {
-    for (int i = 0; i < (sizeof(dswitches)/sizeof(int)); i++) {
-        dswitch_state[i] = digitalRead(dswitches[i]);
+void refreshInput() {
+  switches.turnleft     = digitalRead(5) == HIGH;
+  switches.turnright    = digitalRead(6) == HIGH;
+  switches.thdbleft     = digitalRead(SW_THDB_LEFT) == HIGH;
+  switches.thdbright    = digitalRead(SW_THDB_RIGHT) == HIGH;
+  switches.zero         = digitalRead(SW_ZERO) == HIGH;
+  switches.oet          = digitalRead(SW_OET) == HIGH;
+  switches.kill         = digitalRead(SW_KILL) == HIGH;
+  if (turnstate.turnlong) {
+    if (switches.kill) {
+      turnstate.turnlong = false;
+      turnstate.left = false;
+      turnstate.right = false;
     }
+  } else {
+    turnstate.left = switches.turnleft;
+    turnstate.right = switches.turnright;
+    if (switches.zero) {
+      turnstate.offset = turnstate.angle;
+    }
+    if (switches.oet) {
+      turnstate.turnlong = true;
+      if (turnstate.angle > 0) {
+        turnstate.turnlong = true;
+        turnstate.left = true;
+        turnstate.target = turnstate.angle - 180;
+      } else {
+        turnstate.turnlong = true;
+        turnstate.right = true;
+        turnstate.target = turnstate.angle + 180;
+      }
+    }
+    if (switches.thdbleft) {
+      turnstate.thdbleft = turnstate.angle;
+    }
+    if (switches.thdbright) {
+      turnstate.thdbright = turnstate.angle;
+    }
+    setDirection(turnstate.right);
+  }
 }
 
-void interpretInput() {
-    if (!arrayEquals(dswitch_state, dswitch_state_old) || !arrayEquals(aswitch_state, aswitch_state_old)) {
-        if (!longturn) {
-            turning_left = dswitch_state[SW_TURN_LEFT] == HIGH;
-            turning_right = dswitch_state[SW_TURN_RIGHT] == HIGH;
-            turning = turning_left || turning_right;
-            if (dswitch_state[SW_ZERO] == HIGH) {
-                offset = angle;
-            }
-
-        }
-        if (dswitch_state[SW_KILL] == HIGH) {
-            longturn = false;
-            turning_left = false;
-            turning_right = false;
-            target = angle;
-        }
-    }
-    dswitch_state_old = dswitch_state;
-    aswitch_state_old = aswitch_state;
-}
-
-
-/****************************************************************************************/
-/***************************  MOTOR FUNCTIONS       *************************************/
-/****************************************************************************************/
-
-void setDirection(bool cw) {
-    if (cw) {
-        digitalWrite(CW_PIN, HIGH);
+void pulse() {
+  pulsePin = flipHighLow(pulsePin);
+  digitalWrite(OUT_PULSE, pulsePin);
+  if (pulsePin == LOW) {
+    if (turnstate.right) {
+      turnstate.angle += 0.1;
     } else {
-        digitalWrite(CW_PIN, LOW);
+      turnstate.angle -= 0.1;
     }
-}
-
-void doPulse() {
-    if (turning) {
-        if (pulseState) {
-            digitalWrite(PULSE_PIN, HIGH);
-            pulseState = true;
-        } else {
-            digitalWrite(PULSE_PIN, LOW);
-            pulseState = false;
-        }
+    if (turnstate.turnlong) {
+      if (turnstate.left && turnstate.angle < turnstate.target) {
+        turnstate.turnlong = false;
+        turnstate.left = false;
+      } else if (turnstate.right && turnstate.angle > turnstate.target) {
+        turnstate.turnlong = false;
+        turnstate.right = false;
+      }
     }
+  }
 }
 
-
-/****************************************************************************************/
-/***************************  HELPER FUNCTIONS      *************************************/
-/****************************************************************************************/
-
-/**
- * Checks whether a certain timer has expired already
- * @param    int     timerid    The ID of the timer to check on
- * @return   bool
- */
-bool checkTimer(int timerid) {
-    return ((millis() < timer_last[timerid]) || ((millis() - timer_last[timerid]) >= timer_diff[timerid]));
+void setDirection(bool right) {
+  if (right) {
+    digitalWrite(OUT_CW, HIGH);
+  } else {
+    digitalWrite(OUT_CW, LOW);
+  }
 }
 
-/**
- * Resets the timer by ID
- * @param    int    timerid    The ID of the timer to reset
- */
-void resetTimer(int timerid) {
-    timer_last[timerid] = millis();
+int flipHighLow(int input) {
+  if (input == HIGH) {
+    return LOW;
+  } else if (input == LOW) {
+    return HIGH;
+  } else {
+    return input;
+  }
 }
 
-/**
- * Gives the time until the timer expires
- * @param    int     timerid    ID of the timer
- * @return   int
- */
-int untilTimerExpires(int timerid) {
-    return (millis() - microtimer_last[timerid]);
-}
-
-/**
- * Checks whether a certain microtimer has expired already
- * @param    int     timerid    The ID of the timer to check on
- * @return   bool
- */
-bool checkMicroTimer(int timerid) {
-    return ((micros() < microtimer_last[timerid]) || ((micros() - microtimer_last[timerid]) >= microtimer_diff[timerid]));
-}
-
-/**
- * Resets the microtimer by ID
- * @param    int    timerid    The ID of the timer to reset
- */
-void resetMicroTimer(int timerid) {
-   microtimer_last[timerid] = micros();
-}
-
-/**
- * Checks if a float is in a certain range
- * @param    float  num1      The number to check on
- * @param    float  num2      The reference number
- * @param    float  delta     The maximum difference
- * @return   bool
- */
-bool checkFloat(float num1, float num2, float delta) {
-    return (((num1 - num2) < delta) && ((num1 - num2) > -delta));
-}
-
-
-bool arrayEquals(int[] arr1, int[] arr2) {
-    if (sizeof(arr1) != sizeof(arr2)) return false;
-    for (int i = 0; i < sizeof(arr1)/sizeof(arr1[0]); i++) {
-        if (arr1[i] != arr2[i]) return false;
+boolean checkTimer(int index) {
+  if (timer[index].micro) {
+    if ((timer[index].last + timer[index].interval) <= micros()) {
+      timer[index].last = micros();
+      return true;
     }
-    return true;
+  } else {
+    if ((timer[index].last + timer[index].interval) <= millis()) {
+      timer[index].last = millis();
+      return true;
+    }
+  }
+  return false;
+}
+
+void output() {
+  Serial.print("A: ");
+  Serial.print( turnstate.angle - turnstate.offset );
+  Serial.print(" T: ");
+  Serial.print( turnstate.target - turnstate.offset );
+  Serial.print(" -3dB: ");
+  Serial.print( turnstate.thdbleft );
+  Serial.print(" 3dB: ");
+  Serial.println( turnstate.thdbright );
 }
